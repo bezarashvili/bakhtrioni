658f3f23b6c12fd8520faafc6c47850ceb63a7b6
Alexander <38377677+bezarashvili@users.noreply.github.com>
2023-07-05 00:47:14 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Bakhtrioni</title>
+	<style>
+		body {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	</style>
+</head>
+<body>
+	<img src="https://i.imgur.com/YuDspsb.png"/>
+</body>
+</html>
